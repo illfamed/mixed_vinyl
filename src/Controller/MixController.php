089f29f672c51e6aa6ff0cd831a4043416e304bb
illfamed <generalarmyhf@gmail.com>
2024-10-25 20:16:39 +0200

    ep12 done

--- src/Controller/MixController.php
+++ src/Controller/MixController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\VinylMix;
+use App\Repository\VinylMixRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -30,5 +31,15 @@ class MixController extends AbstractController
       $mix->getId(),
       $mix->getTrackCount()
     ));
+  }
+
+  #[Route('/mix/{id}', name: 'app_mix_show')]
+  public function show($id, VinylMixRepository $mixRepository)
+  {
+    $mix = $mixRepository->find($id);
+
+    return $this->render('mix/show.html.twig', [
+      'mix' => $mix,
+    ]);
   }
 }
